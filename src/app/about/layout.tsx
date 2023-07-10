c7a618e9c6d114c7f3406ff37aeabb5584de3e4c
akamezerax <joaomarcosdasilva1@gmail.com>
2023-07-10 00:34:18 -0300

    feat: update database schema

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Layout = () => {
+    return (
+        <div>
+            About Layout
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const metadata = {
+    
+    title: "About"
+}
+
+const Page = () => {
+    return (
+        <div>
+            Trips
+        </div>
+    )
+}
+
+export default Page
